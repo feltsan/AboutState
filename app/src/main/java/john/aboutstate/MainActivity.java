146b33929cf8e30a9a67274628cb740aa5db0b59
Ivan Feltsan <ivan.feltsan@gmail.com>
2014-11-28 02:17:26 +0200

    Initial commit

---
+++ app/src/main/java/john/aboutstate/MainActivity.java
@@ -0,0 +1,26 @@
+package john.aboutstate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+
+public class MainActivity extends Activity {
+
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.google_map)).getMap();
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.setMyLocationEnabled(true);
+    }
+
+}
